cd76001cb5f0e0e8d0cb14f67925d67394c55f34
zkan <kan@prontomarketing.com>
2017-10-31 18:30:08 +0700

    Initialize Zuora bindings

---
+++ zuora/__init__.py
@@ -0,0 +1,10 @@
+# Zuora Python Bindings
+# Authors:
+# Kan Ouivirach <kan@prontomarketing.com>
+
+# Configuration Variables
+
+api_base = 'https://api.zuora.com'
+zuora_api_access_key_id = None
+zuora_pai_secret_access_key = None
+verify_ssl_certs = False
